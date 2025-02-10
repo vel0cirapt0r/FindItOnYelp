71e6e8cb6f5d7dd4bb2bf4fa9f2e783a94178b52
vel0cirapt0r <nimazabihi2200@gmail.com>
2025-02-10 23:18:49 +0100

    Refactor file handler logging initialization
    - Removed redundant logger instantiation (logger already imported from backend.utils.logger)

--- backend/utils/file_handler.py
+++ backend/utils/file_handler.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from backend.utils.logger import logger
 from backend.models.models import Business, Location, Category, BusinessCategory, BusinessHours, Attribute
 
-logger = logger.getLogger(__name__)
 
 EXPORT_DIR = "exports"
 
